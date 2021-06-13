7f4cff1558b6aa48e8885e08440c1205035d0ddf
Ryo Kitagawa <ryo-kitagawa@m3.com>
2021-06-13 15:36:39 +0900

    Add formula

---
+++ Formula/kj.rb
@@ -0,0 +1,33 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                https://rubydoc.brew.sh/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+class Kj < Formula
+  desc "Edit and create Kubernetes job from cronjob template using your EDITOR"
+  homepage ""
+  url "https://github.com/kitagry/kj/releases/download/0.0.1/kj-0.0.1-darwin-amd64.tar.gz"
+  sha256 "21b08bdc3df1bf6fc907fba7530dd51e5fe413a17c22eecfd8d9a9a55999c124"
+  license ""
+
+  # depends_on "cmake" => :build
+
+  def install
+    # ENV.deparallelize  # if your formula fails when building in parallel
+    # Remove unrecognized options if warned by configure
+    # https://rubydoc.brew.sh/Formula.html#std_configure_args-instance_method
+    system "./configure", *std_configure_args, "--disable-silent-rules"
+    # system "cmake", "-S", ".", "-B", "build", *std_cmake_args
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! For Homebrew/homebrew-core
+    # this will need to be a test that verifies the functionality of the
+    # software. Run the test with `brew test kj`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
+    system "false"
+  end
+end
